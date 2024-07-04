3ba8963c7ebdb91331c2948fbef2522b4f86d041
IvanRuskevych <ivan.ruskevych@gmail.com>
2024-07-05 02:03:59 +0300

    init project & add common settings

---
+++ utils/httpError.js
@@ -0,0 +1,15 @@
+const errorMessageList = {
+    400: 'Bad Request',
+    401: 'Unauthorized',
+    403: 'Forbidden',
+    404: 'Not Found',
+    409: 'Conflict',
+};
+
+const httpError = (status, message = errorMessageList[status]) => {
+    const err = new Error(message);
+    err.status = status;
+    return err;
+};
+
+module.exports = httpError;
