5eb7815c61b6a9be0e981569e139aa987739a311
IvanRuskevych <ivan.ruskevych@gmail.com>
2024-07-08 18:25:28 +0300

    reate get by ID controllers & update services

--- services/auth.service.js
+++
@@ -1,8 +0,0 @@
-const { UserModel } = require('../models');
-
-module.exports.getExistsUser = async (employeeID, password, selectArgs) => await UserModel.exists({ employeeID, password }).select(selectArgs);
-
-module.exports.updateUserDataByID = async (id, accessToken, refreshToken) =>
-  await UserModel.findByIdAndUpdate(id, { accessToken, refreshToken }, { new: true }).select('name phone accessToken refreshToken -_id');
-
-module.exports.getUserById = async (userId, selectArgs) => await UserModel.findById(userId).select(selectArgs);
